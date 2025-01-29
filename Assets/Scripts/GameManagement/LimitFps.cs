150c070c309ffb17e239ec77b7d404357df51939
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2025-01-29 12:00:01 +0100

    [ Temp ] Lock Frame Rate at 60 fps
    faire en sorte que le vitesse de déplacement soit proportionel au frame rate

--- Assets/Scripts/GameManagement/LimitFps.cs
+++ Assets/Scripts/GameManagement/LimitFps.cs
@@ -1,13 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LimitFps : MonoBehaviour
 {
     [SerializeField] private int _maxFrameRate;
+    private float _frameRate;
+    private TMP_Text _text;
     // Start is called before the first frame update
     void Start()
     {
+        _text = GameObject.FindGameObjectWithTag("FrameRate").GetComponent<TMP_Text>();
+        
+        _text.text = "Frame Rate : ";
         Application.targetFrameRate = _maxFrameRate;
+    }
+
+    void Update()
+    {
+        _frameRate = 1f / Time.deltaTime;
+        _text.text = "Frame Rate : " + Mathf.RoundToInt(_frameRate);
     }
 }
